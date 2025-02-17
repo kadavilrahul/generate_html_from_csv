8f4bed6f520f87802cd223baaf859e3172ee96c2
kadavilrahul <kadavil.rahul@gmail.com>
2025-02-17 18:08:44 +0530

    Updated folder names in setup.sh

--- setup.sh
+++ setup.sh
@@ -858,14 +858,13 @@ const path = require('path');
 const ejs = require('ejs');
 
 // Define directories
-const baseDir = '$PROJECT_DIR';
 const outputDir = path.join(baseDir, 'public/products');
 const imagesDir = path.join(baseDir, 'public/images');
 const dataDir = path.join(baseDir, 'data');
 
 // Base URL configurations
-const baseDir = process.env.PWD; // This will get the current working directory
-const folderName = baseDir.split('/').pop(); // Gets the last part of the path
+const baseDir = process.cwd(); // This will get the current working directory
+const folderName = baseDir.split('/').pop(); // Extract folder name
 const BASE_URL = `https://${folderName}`;
 const PRODUCTS_BASE_URL = `${BASE_URL}/public/products`;
 const IMAGES_BASE_URL = `${BASE_URL}/public/images`;
