720808347251e23e05998c738bf6cf8b332e0218
Rahul Dinesh <kadavil.rahul@gmail.com>
2025-09-06 21:43:39 +0530

    Removed older versions

--- version_01/add_timestamps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Generate timestamp
-timestamp=$(date '+%Y%m%d_%H%M%S')
-
-# Rename folder and file
-mv data "data_${timestamp}"
-mv "data_${timestamp}/sitemap.xml" "data_${timestamp}/sitemap_${timestamp}.xml"
